710183f796b75acbbf835e403b280e68662f1ee9
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-04-01 04:34:22 -0500

    feat: drastically simplify implementation by just hijacking spritebatch effect rendering

---
+++ src/RadiantRevival/Common/SmoothLighting/EntityRenderCapture.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Daybreak.Common.Features.Hooks;
+using Daybreak.Common.Rendering;
+using Terraria;
+
+namespace RadiantRevival.Common.SmoothLighting;
+
+internal static class EntityRenderCapture
+{
+    [OnLoad]
+    private static void ApplyHooks()
+    {
+        On_Main.DrawNPCs += DrawNpcs_Scope;
+        On_Main.DrawCachedNPCs += DrawCachedNpcs_Scope;
+    }
+
+    private static void DrawNpcs_Scope(On_Main.orig_DrawNPCs orig, Main self, bool behindTiles)
+    {
+        Scope(() => orig(self, behindTiles));
+    }
+
+    private static void DrawCachedNpcs_Scope(On_Main.orig_DrawCachedNPCs orig, Main self, List<int> npcCache, bool behindTiles)
+    {
+        Scope(() => orig(self, npcCache, behindTiles));
+    }
+
+    private static void Scope(Action callback)
+    {
+        using var _ = new ScopeStateCapture<bool>(ref Main.gameMenu);
+        Main.gameMenu = true;
+
+        Main.spriteBatch.End(out var ss);
+        {
+            using (SmoothLightingRenderer.BeginScope())
+            {
+                Main.spriteBatch.Begin(ss);
+                callback();
+                Main.spriteBatch.End();
+            }
+        }
+        Main.spriteBatch.Begin(ss);
+    }
+}
---
+++ src/RadiantRevival/Common/SmoothLighting/SmoothNpcLighting.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using Daybreak.Common.Features.Hooks;
+using Daybreak.Common.Features.Models;
+using Microsoft.Xna.Framework.Graphics;
+using MonoMod.Cil;
+using RadiantRevival.Core;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace RadiantRevival.Common.SmoothLighting;
+
+/// <summary>
+///     Capable of applying smooth lighting through a shader.
+/// </summary>
+public static class SmoothLightingRenderer
+{
+    private sealed class Data : IStatic<Data>
+    {
+        public required WrapperShaderData<Assets.SmoothLighting.VanillaSmoothLightingSampler.Parameters> EntityLightingShader { get; init; }
+
+        public static Data LoadData(Mod mod)
+        {
+            return Main.RunOnMainThread(
+                () => new Data
+                {
+                    EntityLightingShader = Assets.SmoothLighting.VanillaSmoothLightingSampler.CreateSmoothLightingShader(),
+                }
+            ).GetAwaiter().GetResult();
+        }
+
+        public static void UnloadData(Data data) { }
+    }
+
+    private sealed class ApplicationScope : IDisposable
+    {
+        public ApplicationScope()
+        {
+            currentlyApplied++;
+        }
+
+        public void Dispose()
+        {
+            currentlyApplied--;
+        }
+    }
+
+    public static bool IsCurrentlyApplied => currentlyApplied > 0;
+
+    private static int currentlyApplied;
+
+    public static IDisposable BeginScope()
+    {
+        return new ApplicationScope();
+    }
+
+#pragma warning disable CA2255
+    [ModuleInitializer]
+    public static void ApplyHooks()
+    {
+        MonoModHooks.Add(
+            typeof(SpriteBatch).GetMethod(nameof(SpriteBatch.PrepRenderState), BindingFlags.NonPublic | BindingFlags.Instance),
+            PrepRenderState_InterceptEffect
+        );
+    }
+#pragma warning restore CA2255
+
+    // TODO: Maybe check if == Main.spriteBatch
+    private static void PrepRenderState_InterceptEffect(Action<SpriteBatch> orig, SpriteBatch self)
+    {
+        // PrepRenderState is called in the SpriteBatch constructor in
+        // ImmediateMode and shortly before rendering during a flush in any
+        // other mode.  Both cases guarantee we can set customEffect before it's
+        // used, which is what we need.
+        // I tried hooking SpriteBatch::.ctor but it seemingly wouldn't apply,
+        // maybe due to inlining, and the constructors are used everywhere, so I
+        // can't selectively re-JIT affected methods.
+
+        if (ShouldInterceptEffect(self))
+        {
+            var effect = Data.Instance.EntityLightingShader;
+            {
+                effect.Parameters.light_map = new HlslSampler2D
+                {
+                    Sampler = SamplerState.LinearClamp,
+                    Texture = LightingBuffers.Instance.TotalLightingBuffer.Target,
+                };
+
+                effect.Apply();
+            }
+            self.customEffect = effect.Shader;
+        }
+
+        orig(self);
+    }
+
+    private static bool ShouldInterceptEffect(SpriteBatch sb)
+    {
+        if (!IsCurrentlyApplied)
+        {
+            return false;
+        }
+
+        if (sb != Main.spriteBatch)
+        {
+            return false;
+        }
+
+        if (sb.customEffect is not null)
+        {
+            return false;
+        }
+
+        // TODO: Do we want to keep this?
+        if (sb.blendState != BlendState.AlphaBlend)
+        {
+            return false;
+        }
+
+        var targets = Main.instance.GraphicsDevice.renderTargetBindings
+                          .Where(x => x.RenderTarget is not null)
+                          .Select(x => x.RenderTarget)
+                          .ToArray();
+        if (targets.Length != 0 && (targets.Length != 1 || targets[0] != Main.screenTarget))
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
