e932157ee95cd37ddf12942afb699687c417df12
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-04-05 22:22:03 -0500

    feat: minor refactor, use screenspacetargetpool for static target lifetimes

--- src/RadiantRevival/Common/SmoothLighting/AmbientOcclusion.cs
+++ src/RadiantRevival/Common/SmoothLighting/AmbientOcclusion.cs
@@ -6,20 +6,26 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using MonoMod.Cil;
 using RadiantRevival.Core;
-using System.Diagnostics;
 using Terraria;
 using Terraria.ModLoader;
 
 namespace RadiantRevival.Common.SmoothLighting;
 
 // TODO: Config
+
 public static class AmbientOcclusion
 {
     private sealed class Data : IStatic<Data>
     {
         public required WrapperShaderData<Assets.SmoothLighting.TileBlur.Parameters> HorizontalShader { get; init; }
+
         public required WrapperShaderData<Assets.SmoothLighting.TileBlur.Parameters> VerticalShader { get; init; }
+
         public required WrapperShaderData<Assets.SmoothLighting.AmbientOcclusionSampler.Parameters> MaskShader { get; init; }
+
+        public required RenderTargetLease BlurTarget { get; init; }
+
+        public required RenderTargetLease BlurTargetSwap { get; init; }
 
         public static Data LoadData(Mod mod)
         {
@@ -29,20 +35,37 @@ public static class AmbientOcclusion
                     HorizontalShader = Assets.SmoothLighting.TileBlur.CreateHorizontalShader(),
                     VerticalShader = Assets.SmoothLighting.TileBlur.CreateVerticalShader(),
                     MaskShader = Assets.SmoothLighting.AmbientOcclusionSampler.CreateMaskShader(),
+                    BlurTarget = ScreenspaceTargetPool.Shared.Rent(Main.instance.GraphicsDevice, GetBlurTargetSize),
+                    BlurTargetSwap = ScreenspaceTargetPool.Shared.Rent(Main.instance.GraphicsDevice, GetBlurTargetSize),
                 }
             ).GetAwaiter().GetResult();
+
+            static (int w, int h) GetBlurTargetSize(int width, int height, int targetWidth, int targetHeight)
+            {
+                return (targetWidth / 2, targetHeight / 2);
+            }
         }
 
-        public static void UnloadData(Data data) { }
+        public static void UnloadData(Data data)
+        {
+            Main.RunOnMainThread(
+                () =>
+                {
+                    data.BlurTarget.Dispose();
+                    data.BlurTargetSwap.Dispose();
+                }
+            );
+        }
     }
 
-    private static RenderTargetLease? blurTarget;
+    private static RenderTargetLease BlurTarget => Data.Instance.BlurTarget;
+
+    private static RenderTargetLease BlurTargetSwap => Data.Instance.BlurTargetSwap;
 
     [OnLoad]
     private static void Load()
     {
         On_Main.RenderTiles += RenderTiles_BlurTarget;
-
         IL_Main.DoDraw_WallsAndBlacks += WallsAndBlacks_Occlusion;
     }
 
@@ -53,24 +76,15 @@ public static class AmbientOcclusion
         var horizShader = Data.Instance.HorizontalShader;
         var vertShader = Data.Instance.VerticalShader;
 
-        SpriteBatch sb = Main.spriteBatch;
-        GraphicsDevice device = Main.graphics.GraphicsDevice;
+        var sb = Main.spriteBatch;
+        var device = Main.graphics.GraphicsDevice;
 
         const int sample_count = 16;
 
         var screenSize = new Vector2(Main.screenWidth, Main.screenHeight);
+        var blurSize = new Vector2(16f) / screenSize;
 
-        var blurSize = new Vector2(16) / screenSize;
-
-        var width = Main.tileTarget.Texture.Width / 2;
-        var height = Main.tileTarget.Texture.Height / 2;
-
-        using var blurTargetSwap = RenderTargetPool.Shared.Rent(device, width, height);
-
-        blurTarget?.Dispose();
-        blurTarget = RenderTargetPool.Shared.Rent(device, width, height);
-
-        using (blurTargetSwap.Scope(clearColor: Color.Transparent))
+        using (BlurTargetSwap.Scope(clearColor: Color.Transparent))
         {
             horizShader.Parameters.sample_count = sample_count;
             horizShader.Parameters.blur_size = blurSize;
@@ -78,23 +92,23 @@ public static class AmbientOcclusion
             horizShader.Apply();
 
             sb.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.LinearClamp, DepthStencilState.None, RasterizerState.CullCounterClockwise, horizShader.Shader);
-
-            sb.Draw(Main.tileTarget.Texture, device.Viewport.Bounds, Color.White);
-
+            {
+                sb.Draw(Main.tileTarget.Texture, device.Viewport.Bounds, Color.White);
+            }
             sb.End();
         }
 
-        using (blurTarget.Scope(clearColor: Color.Transparent))
+        using (BlurTarget.Scope(clearColor: Color.Transparent))
         {
             vertShader.Parameters.sample_count = sample_count;
             vertShader.Parameters.blur_size = blurSize;
 
             vertShader.Apply();
 
-            sb.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.LinearClamp, DepthStencilState.None, RasterizerState.CullCounterClockwise, vertShader.Shader); // vertShader.Shader
-
-            sb.Draw(blurTargetSwap.Target, device.Viewport.Bounds, Color.White);
-
+            sb.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.LinearClamp, DepthStencilState.None, RasterizerState.CullCounterClockwise, vertShader.Shader);
+            {
+                sb.Draw(BlurTargetSwap.Target, device.Viewport.Bounds, Color.White);
+            }
             sb.End();
         }
     }
@@ -110,32 +124,23 @@ public static class AmbientOcclusion
             i => i.MatchCallvirt<SpriteBatch>(nameof(SpriteBatch.Begin)),
             i => i.MatchBr(out _)
         );
-
         c.MoveAfterLabels();
 
         c.EmitLdloca(snapshotDef);
-
         c.EmitDelegate(
             static (ref SpriteBatchSnapshot ss) =>
             {
-                Debug.Assert(blurTarget is not null);
-
-                var maskShader = Data.Instance.MaskShader;
-
-                SpriteBatch sb = Main.spriteBatch;
-
+                var sb = Main.spriteBatch;
                 sb.End(out ss);
 
-                Color color = Color.Black * 0.36f;
-
+                var color = Color.Black * 0.36f;
+                var maskShader = Data.Instance.MaskShader;
                 maskShader.Parameters.occlusion_color = color.ToVector4();
-
                 maskShader.Parameters.tile_tex = new HlslSampler2D
                 {
-                    Texture = blurTarget.Target,
-                    Sampler = SamplerState.PointClamp
+                    Texture = BlurTarget.Target,
+                    Sampler = SamplerState.PointClamp,
                 };
-
                 maskShader.Apply();
 
                 sb.Begin(ss with { SortMode = SpriteSortMode.Immediate, CustomEffect = maskShader.Shader });
@@ -148,7 +153,6 @@ public static class AmbientOcclusion
         );
 
         c.EmitLdloca(snapshotDef);
-
         c.EmitDelegate(
             static (ref SpriteBatchSnapshot ss) =>
             {
