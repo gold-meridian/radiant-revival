895b2aa0a0e7b55bc28f4e731ca73952feaae4d6
Zoe <88523932+ZenTheMod@users.noreply.github.com>
2026-04-06 15:27:30 -0700

    hide achievements

---
+++ src/RadiantRevival/Common/HideAchievements.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.CompilerServices;
+using Terraria.Initializers;
+using Terraria.UI;
+
+namespace RadiantRevival.Common;
+
+internal static class HideAchievements
+{
+#pragma warning disable CA2255
+    [ModuleInitializer]
+    public static void Init()
+    {
+        On_AchievementInitializer.OnAchievementCompleted += (_, _) => { };
+        On_InGameNotificationsTracker.AddCompleted += (_, _) => { };
+    }
+#pragma warning restore CA2255
+}
