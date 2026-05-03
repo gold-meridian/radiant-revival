f3a1c049706971c2b64aa4a9b7f6b6385daba481
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-05-02 21:08:26 -0500

    feat: uncapped game zoom

---
+++ src/RenderReprise/Common/BetterZoom/UncappedZoomThresholds.cs
@@ -0,0 +1,29 @@
+﻿using Daybreak.Common.Features.Hooks;
+using Microsoft.Xna.Framework;
+using MonoMod.Cil;
+using Terraria;
+using Terraria.Graphics;
+
+namespace RenderReprise.Common;
+
+internal static class UncappedZoomThresholds
+{
+    [OnLoad]
+    private static void ApplyHooks()
+    {
+        IL_Main.DoDraw += il =>
+        {
+            var c = new ILCursor(il);
+
+            c.GotoNext(MoveType.After, x => x.MatchLdsflda<Main>(nameof(Main.GameViewMatrix)));
+            c.EmitDup();
+            c.EmitDelegate(
+                (ref SpriteViewMatrix gameViewMatrix) =>
+                {
+                    // No more clamping of GameZoomTarget.
+                    gameViewMatrix.Zoom = new Vector2(Main.ForcedMinimumZoom * Main.GameZoomTarget);
+                }
+            );
+        };
+    }
+}
