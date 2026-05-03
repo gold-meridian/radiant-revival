6437b749412ddd94f55dd44203b2f7b0266fb23f
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-05-02 21:19:38 -0500

    feat: uncap ui zoom

--- src/RenderReprise/Common/BetterZoom/UncappedZoomThresholds.cs
+++ src/RenderReprise/Common/BetterZoom/UncappedZoomThresholds.cs
@@ -1,8 +1,12 @@
-﻿using Daybreak.Common.Features.Hooks;
+﻿using System.Reflection;
+using Daybreak.Common.Features.Hooks;
 using Microsoft.Xna.Framework;
 using MonoMod.Cil;
 using Terraria;
+using Terraria.GameContent.UI.States;
 using Terraria.Graphics;
+using Terraria.Initializers;
+using Terraria.ModLoader;
 
 namespace RenderReprise.Common;
 
@@ -11,6 +15,7 @@ internal static class UncappedZoomThresholds
     [OnLoad]
     private static void ApplyHooks()
     {
+        // Game zoom
         IL_Main.DoDraw += il =>
         {
             var c = new ILCursor(il);
@@ -25,5 +30,28 @@ internal static class UncappedZoomThresholds
                 }
             );
         };
+
+        // Interface zoom
+        MonoModHooks.Add(
+            typeof(Main).GetProperty(nameof(Main.UIScaleMax), BindingFlags.Public | BindingFlags.Instance)!.GetGetMethod()!,
+            (Main self) => float.MaxValue
+        );
+        
+        // Uses UIScaleMax
+        MonoModHooks.Modify(
+            typeof(Main).GetProperty(nameof(Main.UIScale), BindingFlags.Public | BindingFlags.Static)!.GetSetMethod()!,
+            _ => { }
+        );
+        
+        // Uses set_UIScale
+        IL_UIWorldGenDebug.Recalculate += _ => { };
+        IL_UIWorldGenDebug.Draw += _ => { };
+        IL_IngameOptions.Draw += _ => { };
+        IL_UILinksInitializer.HandleOptionsSpecials += _ => { };
+        IL_Main.LoadSettings += _ => { };
+        IL_Main.PreDrawMenu += _ => { };
+        IL_Main.FixUIScale += _ => { };
+        
+        // TODO: May also need to check for uses of FixUIScale
     }
 }
