5386e0cdb1506065ffb5fccd5af35fd9a380ec2b
Zoe <88523932+ink-zoey@users.noreply.github.com>
2026-06-04 19:51:57 -0700

    godrays rev grav support

--- src/RadiantRevival/Common/Rendering/Godrays.cs
+++ src/RadiantRevival/Common/Rendering/Godrays.cs
@@ -1,29 +1,55 @@
-﻿using System;
-using System.Diagnostics;
-using Daybreak.Common.Features.Hooks;
+﻿using Daybreak.Common.Features.Hooks;
+using Daybreak.Common.Features.Models;
 using Daybreak.Common.Rendering;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using RadiantRevival.Core;
+using System;
 using Terraria;
 using Terraria.GameContent.Drawing;
+using Terraria.ModLoader;
 
 namespace RadiantRevival.Common;
 
 // TODO: Config, DrawCapture
 public static class Godrays
 {
-    private static WrapperShaderData<Assets.Sky.Godrays.Parameters>? godraysShaderData;
-    private static WrapperShaderData<Assets.Sky.GodraysSampler.Parameters>? blurShaderData;
+    private sealed class Data : IStatic<Data>
+    {
+        public required WrapperShaderData<Assets.Sky.Godrays.Parameters> GodraysShader { get; init; }
 
-    private static RenderTargetLease? celestialBodyLease;
+        public required WrapperShaderData<Assets.Sky.GodraysSampler.Parameters> BlurShader { get; init; }
+
+        public required RenderTargetLease CelestialBodyTarget { get; init; }
+
+        public static Data LoadData(Mod mod)
+        {
+            return Main.RunOnMainThread(
+                () => new Data
+                {
+                    GodraysShader = Assets.Sky.Godrays.CreateGodraysShader(),
+                    BlurShader = Assets.Sky.GodraysSampler.CreateRadialBlurShader(),
+                    CelestialBodyTarget = ScreenspaceTargetPool.Shared.Rent(Main.instance.GraphicsDevice),
+                }
+            ).GetAwaiter().GetResult();
+        }
+
+        public static void UnloadData(Data data)
+        {
+            Main.RunOnMainThread(
+                () =>
+                {
+                    data.CelestialBodyTarget.Dispose();
+                }
+            );
+        }
+    }
+
+    private static RenderTargetLease CelestialBodyTarget => Data.Instance.CelestialBodyTarget;
 
     [OnLoad]
     private static void Load()
     {
-        godraysShaderData = Assets.Sky.Godrays.CreateGodraysShader();
-        blurShaderData = Assets.Sky.GodraysSampler.CreateRadialBlurShader();
-
         On_Main.DrawSunAndMoon += DrawSunAndMoon_CaptureCelestialBodies;
 
         On_Main.DrawLensFlare += DrawLensFlare_Godrays;
@@ -38,24 +64,25 @@ public static class Godrays
             return;
         }
 
-        celestialBodyLease?.Dispose();
-        celestialBodyLease = ScreenspaceTargetPool.Shared.Rent(self.GraphicsDevice);
-
-        using (celestialBodyLease.Scope(clearColor: Color.Transparent))
+        using (CelestialBodyTarget.Scope(clearColor: Color.Transparent))
         {
             orig(self, sceneArea, moonColor, sunColor, tempMushroomInfluence);
         }
 
-        using var _ = Main.spriteBatch.Scope();
+        var sb = Main.spriteBatch;
 
-        Main.spriteBatch.Begin();
+        using var _ = sb.Scope();
 
-        Main.spriteBatch.Draw(celestialBodyLease.Target, Vector2.Zero, Color.White);
+        sb.Begin();
+        {
+            sb.Draw(CelestialBodyTarget.Target, Vector2.Zero, Color.White);
+        }
+        sb.End();
     }
 
     private static void DrawLensFlare_Godrays(On_Main.orig_DrawLensFlare orig)
     {
-        if (!Main.dayTime || !Main.ForegroundSunlightEffects || Main.screenTarget is null || celestialBodyLease is null)
+        if (!Main.dayTime || !Main.ForegroundSunlightEffects || Main.screenTarget is null)
         {
             orig();
             return;
@@ -68,15 +95,21 @@ public static class Godrays
 
     private static void Draw(SpriteBatch sb, GraphicsDevice device, RenderTarget2D target)
     {
-        Debug.Assert(godraysShaderData is not null && blurShaderData is not null && celestialBodyLease is not null);
-
         const int godrays_samples = 32;
         const int radial_blur_samples = 16;
         const float radial_blur_strength = 0.25f;
 
+        var godraysShader = Data.Instance.GodraysShader;
+        var blurShader = Data.Instance.BlurShader;
+
         var screenSize = new Vector2(Main.screenWidth, Main.screenHeight);
 
         var lightPosition = Main.LastCelestialBodyPosition * screenSize;
+
+        if (Main.GameViewMatrix.Effects.HasFlag(SpriteEffects.FlipVertically))
+        {
+            lightPosition.Y = screenSize.Y - lightPosition.Y;
+        }
 
         HorizonHelper.GetCelestialBodyColors(out var sunColor, out var _);
 
@@ -102,36 +135,35 @@ public static class Godrays
         using (lease.Scope(clearColor: Color.Transparent))
         {
             sb.Begin(SpriteSortMode.Immediate, BlendState.Additive, SamplerState.LinearClamp, DepthStencilState.None, RasterizerState.CullCounterClockwise);
-
-            godraysShaderData.Parameters.LightPosition = lightPosition;
-            godraysShaderData.Parameters.SampleCount = godrays_samples;
-            godraysShaderData.Parameters.DecayMult = 0.92f;
-
-            godraysShaderData.Parameters.LightsTexture = new HlslSampler2D
             {
-                Texture = celestialBodyLease.Target,
-                Sampler = SamplerState.LinearClamp,
-            };
+                godraysShader.Parameters.LightPosition = lightPosition;
+                godraysShader.Parameters.SampleCount = godrays_samples;
+                godraysShader.Parameters.DecayMult = 0.92f;
 
-            godraysShaderData.Apply();
+                godraysShader.Parameters.LightsTexture = new HlslSampler2D
+                {
+                    Texture = CelestialBodyTarget.Target,
+                    Sampler = SamplerState.LinearClamp,
+                };
 
-            sb.Draw(target, device.Viewport.Bounds, color);
+                godraysShader.Apply();
+
+                sb.Draw(target, device.Viewport.Bounds, color);
+            }
             sb.End();
         }
 
         sb.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.LinearClamp, DepthStencilState.None, RasterizerState.CullCounterClockwise);
+        {
+            blurShader.Parameters.LightPosition = lightPosition;
+            blurShader.Parameters.SampleCount = radial_blur_samples;
+            blurShader.Parameters.BlurStrength = radial_blur_strength;
 
-        blurShaderData.Parameters.LightPosition = lightPosition;
-        blurShaderData.Parameters.SampleCount = radial_blur_samples;
-        blurShaderData.Parameters.BlurStrength = radial_blur_strength;
+            blurShader.Apply();
 
-        blurShaderData.Apply();
-
-        sb.Draw(lease.Target, device.Viewport.Bounds, Color.White);
-
+            sb.Draw(lease.Target, device.Viewport.Bounds, Color.White);
+        }
         sb.End();
 
-        celestialBodyLease.Dispose();
-        celestialBodyLease = null;
     }
 }
