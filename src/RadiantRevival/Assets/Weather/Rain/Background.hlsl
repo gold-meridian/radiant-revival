3f6c911a16dcfbb17bfe54ed552d695200e59538
Zoe <88523932+ink-zoey@users.noreply.github.com>
2026-06-09 19:52:01 -0700

    something about transparent rain

--- src/RadiantRevival/Assets/Weather/Rain/Background.hlsl
+++ src/RadiantRevival/Assets/Weather/Rain/Background.hlsl
@@ -1,9 +1,12 @@
 ﻿#include "../../common.h"
 
+#define TILE_SIZE (16.0)
+
+#define RAIN_ALPHA (0.2)
+#define RAIN_INTENSITY_MIN (0.45)
+
 sampler2D RainTexture : register(s0);
 sampler2D LightMap : register(s1);
-
-#define TILE_SIZE (16.0)
 
 SCREEN_SIZE(ScreenSize);
 TEXTURE_SIZE(LightingBufferSize, 1);
@@ -26,7 +29,7 @@ float4 BackgroundShaderFragment(float2 uv : TEXCOORD0, float2 svPos : SV_POSITIO
     float2 rainUv = uv;
     float4 rain = tex2D(RainTexture, rainUv);
     
-    rain *= 0.4 * max(1 - pow(Intensity, 2), 0.4) * GlobalBrightness;
+    rain *= RAIN_ALPHA * max(1 - pow(Intensity, 2), RAIN_INTENSITY_MIN) * GlobalBrightness;
     rain.rgb *= light * baseColor.rgb;
     
     rain *= rain.a;
--- src/RadiantRevival/Assets/Weather/Rain/Distortion.hlsl
+++ src/RadiantRevival/Assets/Weather/Rain/Distortion.hlsl
@@ -1,12 +1,17 @@
 ﻿#include "../../common.h"
 
+#define TILE_SIZE (16.0)
+#define DISTORTION_SIZE (8.0)
+
+#define RAIN_COLOR_MULTIPLIER (0.25)
+#define RAIN_COLOR_ADD (0.15)
+#define RAIN_INTENSITY_MIN (0.45)
+
 sampler2D ScreenTexture : register(s0);
 sampler2D MaskTexture : register(s1);
 sampler2D LightMap : register(s2);
 sampler2D RainTexture : register(s3);
 sampler2D NoiseTexture : register(s4);
-
-#define TILE_SIZE (16.0)
 
 SCREEN_SIZE(ScreenSize);
 TEXTURE_SIZE(MaskTextureSize, 1);
@@ -54,7 +59,7 @@ float4 DistortionShaderFragment(float2 svPos : SV_POSITION, float4 baseColor : C
         lightUv += 0.5;
     }
     
-    float mask = tex2D(MaskTexture, maskUv);
+    float mask = 1 - tex2D(MaskTexture, maskUv);
     
     float3 light = tex2D(LightMap, lightUv);
     
@@ -75,12 +80,19 @@ float4 DistortionShaderFragment(float2 svPos : SV_POSITION, float4 baseColor : C
     
     float noise = pow(tex2D(NoiseTexture, noiseUv).r, 3) * 4;
     
-    float offsetSize = 8 * (Intensity + noise) / max(ScreenSize.x, ScreenSize.y);
-    float2 offset = -Direction * offsetSize * (1 - pow(1 - rain.a, 5)) * (1 - mask);
+    float offsetSize = DISTORTION_SIZE * (1 - pow(1 - Intensity, 4) + noise) / max(ScreenSize.x, ScreenSize.y);
+    float2 offset = -Direction * offsetSize * (1 - pow(1 - rain.a, 5)) * mask;
     
-    float4 color = tex2D(ScreenTexture, (uv) + offset);
+    float2 distortedUv = uv + offset;
     
-    color.rgb += rain.rgb * rain.a * (1 - mask) * 0.24 * max(1 - pow(Intensity, 2), 0.4) * light * GlobalBrightness;
+    float4 color = tex2D(ScreenTexture, distortedUv);
+    
+    float rainAlpha = rain.a * mask * max(1 - pow(Intensity, 2), RAIN_INTENSITY_MIN) * GlobalBrightness;
+    
+    float3 rainColor = rain.rgb;
+    
+    color.rgb = lerp(color.rgb, color.rgb * rainColor, rainAlpha * RAIN_COLOR_MULTIPLIER);
+    color.rgb += rainColor * light * rainAlpha * RAIN_COLOR_ADD;
     
     return color;
 }
