134b853f351afd9aa8dd3ed5f4c6f60e2fa439ff
Lucille Karma <farfetchedking541@gmail.com>
2026-06-19 16:35:04 -0500

    chore: documentation for rainbow shader
    Sorry, I forgot to do this earlier before making the pull request.

--- src/RadiantRevival/Assets/Sky/RainbowShader.hlsl
+++ src/RadiantRevival/Assets/Sky/RainbowShader.hlsl
@@ -76,8 +76,43 @@ float3 CalculateColorFromWavelength(float wavelength)
 float Sample(float wavelengthNanometers, float theta)
 {
     float refractiveIndex = CalculateRefractiveIndex(wavelengthNanometers * 0.001);
-        
-    float angularDistance = distance(asin(1 / refractiveIndex) * 6 - 3.141, theta);
+    
+    // The asin(1 / n) * 6 - pi is derived by applying
+    // numerical optimization from an angular deviation function
+    // in accordance with Snell's Law:
+    
+    // D(a) = 2a - 4b + pi
+    
+    // In this context, a is the angle of incidence, and
+    // b is the angle of refraction.
+    // You'll just have to trust me on this one or do some
+    // googling, but this deviation function is basically
+    // derived from the internal reflection angles
+    // in an assumedly perfectly spherical droplet of water.
+    
+    // Note that the angle of incidence comes from air, and
+    // the angle of reflection enters water.
+    // For the purposes of this derivation, the index of refraction
+    // for air is assumed to be one.
+    
+    // Using Snell's law provides the following derivation:
+    // sin(a) = n * sin(b)
+    // b(a) = arcsin(sin(a) / n)
+    // D(a) = 2a - 4 * arcsin(sin(a) / n) + pi
+    
+    // From here, it's a matter of finding the minimum of D(a), or
+    // dD/da = 0. This is where the aforementioned optimization comes
+    // into play.
+    
+    // I'll skip the calculus here because I'm lazy and my
+    // fingers hurt slightly from typing this all out already, but
+    // the result of such, after a moderate amount of work with
+    // algebraic and trigonometric reductions, gives back
+    // a rainbow angle of 6 * arcsin(1 / n) - pi.
+    
+    // From there, all that needs to be done is checking the
+    // viewing angle against it.
+    float angularDistance = distance(asin(1 / refractiveIndex) * 6 - 3.141, theta);    
     float intensity = exp(-pow(angularDistance, 2) / 0.0002);
     
     return intensity;
@@ -85,18 +120,23 @@ float Sample(float wavelengthNanometers, float theta)
 
 float4 PixelShaderFunction(float4 sampleColor : COLOR0, float2 uv : TEXCOORD0) : COLOR0
 {
+    // Standard UV to world position stuff.
+    // The origin of the rainbow is biased horizontally
+    // towards the sun in order to make it look better.
     float2 worldStableUv = (uv - 0.5) / zoom + 0.5 + screenPosition / screenSize;
     float2 worldPosition = worldStableUv * screenSize;
-    
     float2 origin = screenPosition + screenSize * float2(0.5, 0.9);
     origin.x = lerp(origin.x, sunPosition.x, 0.525);
     
-    float3 viewDirection = normalize(float3(worldPosition - origin, 0));
-    
+    float3 viewDirection = normalize(float3(worldPosition - origin, 0));    
     float3 sunDirection = normalize(sunPosition - float3(worldPosition, 0));
     float sunViewOrthogonality = dot(sunDirection, viewDirection);
     float theta = acos(sunViewOrthogonality);
     
+    // Apply step-wise increments along the visible
+    // color spectrum, checking the viewing angle
+    // against the rainbow angle, as determined via
+    // the Sample function.
     float3 color = 0;
     float intensitySum = 0;
     for (float i = 0; i < 16; i++)
