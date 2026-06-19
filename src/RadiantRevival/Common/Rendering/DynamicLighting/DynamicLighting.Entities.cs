e3ebf977457e69d90738f683253fae13a5f6602f
Zoe <88523932+ink-zoey@users.noreply.github.com>
2026-06-18 19:02:22 -0700

    stub impl

---
+++ src/RadiantRevival/Common/Rendering/DynamicLighting/DynamicLighting.Entities.cs
@@ -0,0 +1,48 @@
+﻿using Daybreak.Common.Features.Hooks;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Graphics.Light;
+using Terraria.ID;
+using Terraria.Utilities;
+
+namespace RadiantRevival.Common;
+
+public static class CommonEntityDynamicLighting
+{
+    [OnLoad]
+    private static void Load()
+    {
+        On_Projectile.Update += (orig, self, i) =>
+        {
+            DynamicLighting.Scope(() => orig(self, i));
+        };
+
+        On_Player.Update += (orig, self, i) =>
+        {
+            DynamicLighting.Scope(() => orig(self, i));
+        };
+
+        On_TileLightScanner.ApplyTileLight += ApplyTileLight_DynamicLighting;
+    }
+
+    private static void ApplyTileLight_DynamicLighting(On_TileLightScanner.orig_ApplyTileLight orig, TileLightScanner self, Tile tile, int x, int y, ref FastRandom localRandom, ref Vector3 lightColor)
+    {
+        var type = tile.TileType;
+
+        if (TileID.Sets.Torches[type] ||
+            TileID.Sets.RoomNeeds.CountsAsTorch[type] ||
+            // TileID.Sets.RoomNeeds_Vanilla.CountsAsTorch[type] || Unused?
+            TileID.Sets.Campfires[type]
+        )
+        {
+            using (DynamicLighting.BeginScope())
+            {
+                orig(self, tile, x, y, ref localRandom, ref lightColor);
+            }
+
+            return;
+        }
+
+        orig(self, tile, x, y, ref localRandom, ref lightColor);
+    }
+}
---
+++ src/RadiantRevival/Common/Rendering/DynamicLighting/DynamicLighting.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.Xna.Framework;
+using MonoMod.Cil;
+using System;
+using Terraria;
+using Terraria.GameContent.Drawing;
+using Terraria.GameContent.LeashedEntities;
+using Terraria.Graphics.Light;
+
+namespace RadiantRevival.Common;
+
+public static class DynamicLighting
+{
+    private static bool currentlyApplied;
+
+    private sealed class ApplicationScope : IDisposable
+    {
+        private readonly bool wasApplied;
+
+        public ApplicationScope()
+        {
+            wasApplied = currentlyApplied;
+            currentlyApplied = true;
+        }
+
+        public void Dispose()
+        {
+            currentlyApplied = wasApplied;
+        }
+    }
+
+    public static IDisposable BeginScope()
+    {
+        return new ApplicationScope();
+    }
+
+    public static void Scope(Action callback)
+    {
+        using (BeginScope())
+        {
+            callback();
+        }
+    }
+
+    private static void Load()
+    {
+        IL_Lighting.AddLight_int_int_float_float_float += AddLight_DynamicLighting;
+        {
+            IL_DelegateMethods.CastLight += _ => { };
+            IL_DelegateMethods.CastLightOpen += _ => { };
+            IL_DelegateMethods.CastLightOpen_StopForSolids_ScaleWithDistance += _ => { };
+            IL_DelegateMethods.CastLightOpen_StopForSolids += _ => { };
+            IL_DelegateMethods.SpreadLightOpen_StopForSolids += _ => { };
+            IL_Gore.Update += _ => { };
+            IL_Lighting.AddLight_Vector2_Vector3 += _ => { };
+            IL_Lighting.AddLight_Vector2_float_float_float += _ => { };
+            IL_Lighting.AddLight_Vector2_int += _ => { };
+            IL_Main.DrawProj_DrawExtras += _ => { };
+            IL_Mount.Hover += _ => { };
+            IL_Mount.UpdateFrame += _ => { };
+            IL_Mount.UpdateEffects += _ => { };
+            IL_Mount.AimAbility += _ => { };
+            IL_NPC.VanillaAI_Inner += _ => { };
+            IL_NPC.AI_065_Butterflies += _ => { };
+            IL_NPC.AI_037_Destroyer += _ => { };
+            IL_NPC.AI_005_EaterOfSouls += _ => { };
+            IL_NPC.AI_002_FloatingEye += _ => { };
+            IL_NPC.AI_003_Fighters += _ => { };
+            IL_NPC.AI_001_Slimes += _ => { };
+            IL_NPC.UpdateNPC_UpdateTrails += _ => { };
+            IL_NPC.UpdateNPC_BuffApplyVFX += _ => { };
+            IL_NPC.UpdateNPC_CastLights += _ => { };
+            IL_Player.UpdateBuffs += _ => { };
+            IL_Player.ApplyEquipFunctional += _ => { };
+            IL_Player.Update += _ => { };
+            IL_Player.UpdateArmorLights += _ => { };
+            IL_Player.PlayerFrame += _ => { };
+            IL_Player.ItemCheck_EmitUseVisuals += _ => { };
+            IL_Projectile.ProjLight += _ => { };
+            IL_Projectile.VanillaAI += _ => { };
+            IL_Projectile.AI_003_Boomerang += _ => { };
+            IL_Projectile.AI_001 += _ => { };
+            IL_Projectile.AI_026 += _ => { };
+            IL_Projectile.AI_075 += _ => { };
+            IL_WaterfallManager.AddLight += _ => { };
+            IL_WorldItem.UpdateItem_VisualEffects += _ => { };
+            IL_FireflyLeashedCritter.AddLight += _ => { };
+            IL_HellButterflyLeashedCritter.VisualEffects += _ => { };
+            IL_SnailLeashedCritter.VisualEffects += _ => { };
+            IL_TileDrawing.DrawTrees += _ => { };
+        }
+
+        IL_Lighting.AddLight_int_int_int_float += AddLight_DynamicLighting;
+        IL_Dust.UpdateDust += _ => { };
+    }
+
+    private static void AddLight_DynamicLighting(ILContext il)
+    {
+        var c = new ILCursor(il);
+
+        var jumpDynamicLightsTarget = c.DefineLabel();
+
+        c.GotoNext(
+            MoveType.Before,
+            i => i.MatchCallvirt<ILightingEngine>(nameof(ILightingEngine.AddLight))
+        );
+
+        c.EmitDelegate(static () => currentlyApplied);
+
+        c.EmitBrfalse(jumpDynamicLightsTarget);
+
+        c.EmitDelegate(AddDynamicLight);
+        c.EmitPop();
+        c.EmitRet();
+
+        c.MarkLabel(jumpDynamicLightsTarget);
+    }
+
+    public static void AddDynamicLight(int i, int j, Vector3 color)
+    {
+
+    }
+}
