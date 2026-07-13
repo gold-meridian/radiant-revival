c5ada05727f190909caa8b50070fb188b4e26bb9
Zoe <88523932+ink-zoey@users.noreply.github.com>
2026-07-12 22:58:04 -0700

    testing lighting visuals

--- src/RadiantRevival/Assets/Nature/NatureLighting.hlsl
+++ src/RadiantRevival/Assets/Nature/NatureLighting.hlsl
@@ -11,54 +11,57 @@ float MaxSat;
 float MinHue;
 float MaxHue;
 
+float4 LightColor;
+
+float3 RGBtoHCV(float3 color)
+{
+    float4 p = color.g < color.b
+        ? float4(color.bg, -1, 0.6666)
+        : float4(color.gb, 0, -0.3333);
+    
+    float4 q = color.r < p.x
+        ? float4(p.xyw, color.r)
+        : float4(color.r, p.yzx);
+    
+    float C = q.x - min(q.w, q.y);
+    
+    float hue = abs((q.w - q.y) / (6 * C + EPSILON) + q.z);
+    
+    return float3(hue, C, q.x);
+}
+
 float3 RGBtoHSL(float3 color)
 {
-    float highest = max(max(color.r, color.g), color.b);
-    float lowest = min(min(color.r, color.g), color.b);
-
-    float range = highest - lowest;
-
-    float r2 = (highest - color.r) / range;
-    float g2 = (highest - color.g) / range;
-    float b2 = (highest - color.b) / range;
-
-    float hue = 0;
+    float3 hcv = RGBtoHCV(color);
     
-    if (color.r == highest)
-    {
-        hue = (color.g == lowest ? 5 + b2 : 1 - g2) * 0.1666;
-    }
-    else if (color.g == highest)
-    {
-        hue = (color.b == lowest ? 1 + r2 : 3 - b2) * 0.1666;
-    }
-    else
-    {
-        hue = (color.r == lowest ? 3 + g2 : 5 - r2) * 0.1666;
-    }
-
-    float lightness = (lowest + highest) * 0.5;
-
-    float saturation = range / (lightness <= 0.5 ? (highest + lowest) : (2 - highest - lowest));
-
-    return float3(hue, saturation, lightness);
+    float l = hcv.z - hcv.y * 0.5;
+    float s = hcv.y / (1 - abs((l * 2) - 1) + EPSILON);
+    
+    return float3(hcv.x, s, l);
 }
 
 float4 NatureLightingShaderFragment(float2 textureUv : TEXCOORD0, float4 baseColor : COLOR0) : COLOR0
 {
+    const float hue_leniance = 0.0;
+    const float saturation_leniance = 0.0;
+
     float4 base = tex2D(TileTexture, textureUv) * baseColor;
-    
-    clip(base.a - EPSILON);
     
     float4 source = tex2D(TileTexture, textureUv);
     
     float3 hsl = RGBtoHSL(source.rgb);
     
     bool inRange =
-        hsl.x > MinHue && hsl.x < MaxHue
-     && hsl.y > MinSat && hsl.y < MaxSat;
+        hsl.x > max(MinHue - hue_leniance, 0) && hsl.x < min(MaxHue + hue_leniance, 1)
+     && hsl.y > max(MinSat - saturation_leniance, 0) && hsl.y < min(MaxSat + saturation_leniance, 1);
     
-    return base + inRange;
+    float lightness = (hsl.z - 0.2) * 2.3;
+    
+    lightness = pow(1 - lightness, 6) * 0.7;
+    
+    float4 light = LightColor * lightness * inRange * base.a;
+    
+    return base + light;
 }
 
 BEGIN_TECHNIQUE(Technique1)
