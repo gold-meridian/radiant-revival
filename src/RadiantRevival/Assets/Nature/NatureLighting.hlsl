665fef6da9fba14808bfe27de1a36d31487d49a7
Zoe <88523932+ink-zoey@users.noreply.github.com>
2026-07-12 21:37:48 -0700

    arbitrary nature shader with huesat mask

---
+++ src/RadiantRevival/Assets/Nature/NatureLighting.hlsl
@@ -0,0 +1,68 @@
+﻿#include "../common.h"
+
+sampler2D TileTexture : register(s0);
+sampler2D SourceTexture : register(s1);
+
+#define EPSILON (1e-10)
+
+float MinSat;
+float MaxSat;
+
+float MinHue;
+float MaxHue;
+
+float3 RGBtoHSL(float3 color)
+{
+    float highest = max(max(color.r, color.g), color.b);
+    float lowest = min(min(color.r, color.g), color.b);
+
+    float range = highest - lowest;
+
+    float r2 = (highest - color.r) / range;
+    float g2 = (highest - color.g) / range;
+    float b2 = (highest - color.b) / range;
+
+    float hue = 0;
+    
+    if (color.r == highest)
+    {
+        hue = (color.g == lowest ? 5 + b2 : 1 - g2) * 0.1666;
+    }
+    else if (color.g == highest)
+    {
+        hue = (color.b == lowest ? 1 + r2 : 3 - b2) * 0.1666;
+    }
+    else
+    {
+        hue = (color.r == lowest ? 3 + g2 : 5 - r2) * 0.1666;
+    }
+
+    float lightness = (lowest + highest) * 0.5;
+
+    float saturation = range / (lightness <= 0.5 ? (highest + lowest) : (2 - highest - lowest));
+
+    return float3(hue, saturation, lightness);
+}
+
+float4 NatureLightingShaderFragment(float2 textureUv : TEXCOORD0, float4 baseColor : COLOR0) : COLOR0
+{
+    float4 base = tex2D(TileTexture, textureUv) * baseColor;
+    
+    clip(base.a - EPSILON);
+    
+    float4 source = tex2D(TileTexture, textureUv);
+    
+    float3 hsl = RGBtoHSL(source.rgb);
+    
+    bool inRange =
+        hsl.x > MinHue && hsl.x < MaxHue
+     && hsl.y > MinSat && hsl.y < MaxSat;
+    
+    return base + inRange;
+}
+
+BEGIN_TECHNIQUE(Technique1)
+    BEGIN_PASS(NatureLightingShader) 
+        PIXEL_SHADER(compile ps_3_0 NatureLightingShaderFragment()) 
+    END_PASS
+END_TECHNIQUE
--- src/RadiantRevival/Common/Rendering/Nature/NatureLighting.cs
+++ src/RadiantRevival/Common/Rendering/Nature/NatureLighting.cs
@@ -1,30 +1,181 @@
 ﻿using Daybreak.Common.Features.Hooks;
-using MonoMod.Cil;
+using Daybreak.Common.Features.Models;
+using Daybreak.Common.Mathematics;
+using Daybreak.Common.Rendering;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using RadiantRevival.Core;
+using System.Collections.Generic;
+using System.Linq;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent;
 using Terraria.GameContent.Drawing;
+using Terraria.ModLoader;
 
 namespace RadiantRevival.Common;
 
 public static class NatureLighting
 {
+    private sealed class Data : IStatic<Data>
+    {
+        public required WrapperShaderData<Assets.Nature.NatureLighting.Parameters> NatureLightingShader { get; init; }
+
+        public static Data LoadData(Mod mod)
+        {
+            return Main.RunOnMainThread(
+                () => new Data
+                {
+                    NatureLightingShader = Assets.Nature.NatureLighting.CreateNatureLightingShader(),
+                }
+            ).GetAwaiter().GetResult();
+        }
+
+        public static void UnloadData(Data data)
+        { }
+    }
+
+    private record struct NatureData(
+        DrawParameters DrawData,
+        Texture2D? UnpaintedTexture,
+        TreePaintingSettings? TreeSettings,
+        bool IgnoreLighting
+    );
+
+    private sealed class NatureRenderer : INatureRenderer
+    {
+        private readonly List<NatureData> drawData = [];
+
+        public void DrawNature(Texture2D texture, Vector2 position, Rectangle sourceRectangle, Color color, float rotation, Vector2 origin, float scale, SpriteEffects effects, float layerDepth, SideFlags seams = SideFlags.None)
+        {
+            var data = new NatureData(
+                new DrawParameters(texture)
+                {
+                    Position = position,
+                    Source = sourceRectangle,
+                    Color = color,
+                    Rotation = Angle.FromRadians(rotation),
+                    Origin = origin,
+                    Scale = new Vector2(scale),
+                    Effects = effects,
+                },
+                baseTexture,
+                priorSettings,
+                false
+            );
+
+            drawData.Add(data);
+
+            baseTexture = null;
+            priorSettings = null;
+        }
+
+        public void DrawGlowmask(Texture2D texture, Vector2 position, Rectangle? sourceRectangle, Color color, float rotation, Vector2 origin, float scale, SpriteEffects effects, float layerDepth)
+        {
+            var data = new NatureData(
+                new DrawParameters(texture)
+                {
+                    Position = position,
+                    Source = sourceRectangle,
+                    Color = color,
+                    Rotation = Angle.FromRadians(rotation),
+                    Origin = origin,
+                    Scale = new Vector2(scale),
+                    Effects = effects,
+                },
+                null,
+                null,
+                true
+            );
+
+            drawData.Add(data);
+        }
+
+        public void DrawAfterAllObjects(SpriteBatchBeginner beginner)
+        {
+            DrawNatureData(drawData);
+
+            drawData.Clear();
+        }
+    }
+
+    private static Texture2D? baseTexture;
+    private static TreePaintingSettings? priorSettings;
+
     [OnLoad]
     private static void Load()
     {
-        IL_NextNatureRenderer.DrawAfterAllObjects += DrawAfterAllObjects_RemoveGlowmask;
+        Main.instance.TilesRenderer._natureRenderer = new NatureRenderer();
+
+        On_TileDrawing.GetTreeTopTexture += GetTreeTopTexture_GetBaseTexture;
+        On_TileDrawing.GetTreeBranchTexture += GetTreeBranchTexture_GetBaseTexture;
+
+        On_TileDrawing.GetTileDrawTexture_TileVariationkey += GetTileDrawTexture_GetBaseTexture;
     }
 
-    private static void DrawAfterAllObjects_RemoveGlowmask(ILContext il)
+    private static Texture2D GetTileDrawTexture_GetBaseTexture(On_TileDrawing.orig_GetTileDrawTexture_TileVariationkey orig, TileDrawing self, TilePaintSystemV2.TileVariationkey key)
     {
-        var c = new ILCursor(il);
+        baseTexture = TextureAssets.Tile[key.TileType].Value;
 
-        c.GotoNext(
-            MoveType.Before,
-            i => i.MatchLdcR4(0f),
-            i => i.MatchBneUn(out _)
-        );
+        return orig(self, key);
+    }
 
-        c.EmitPop();
+    private static Texture2D GetTreeBranchTexture_GetBaseTexture(On_TileDrawing.orig_GetTreeBranchTexture orig, TileDrawing self, int treeTextureIndex, int treeTextureStyle, byte tileColor)
+    {
+        baseTexture = TextureAssets.TreeBranch[treeTextureIndex].Value;
+        priorSettings = TreePaintSystemData.GetTreeFoliageSettings(treeTextureIndex, treeTextureStyle);
 
-        c.EmitLdcR4(0f);
+        return orig(self, treeTextureIndex, treeTextureStyle, tileColor);
+    }
+
+    private static Texture2D GetTreeTopTexture_GetBaseTexture(On_TileDrawing.orig_GetTreeTopTexture orig, TileDrawing self, int treeTextureIndex, int treeTextureStyle, byte tileColor)
+    {
+        baseTexture = TextureAssets.TreeTop[treeTextureIndex].Value;
+        priorSettings = TreePaintSystemData.GetTreeFoliageSettings(treeTextureIndex, treeTextureStyle);
+
+        return orig(self, treeTextureIndex, treeTextureStyle, tileColor);
+    }
+
+    private static void DrawNatureData(IEnumerable<NatureData> data)
+    {
+        var sb = Main.spriteBatch;
+
+        using var _ = sb.Scope();
+
+        var effect = Data.Instance.NatureLightingShader;
+
+        sb.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.None, Main.Rasterizer, null, Main.Transform);
+        {
+            foreach (var (drawData, unpainted, treeSettings, ignoreLighting) in data)
+            {
+                if (ignoreLighting ||
+                    unpainted is null)
+                {
+                    sb.spriteEffect.CurrentTechnique.Passes[0].Apply();
+
+                    sb.Draw(drawData);
+
+                    continue;
+                }
+
+                effect.Parameters.SourceTexture = new HlslSampler2D
+                {
+                    Sampler = SamplerState.PointClamp,
+                    Texture = unpainted,
+                };
+
+                effect.Parameters.MinSat = treeSettings?.SpecialGroupMinimumSaturationValue ?? 0;
+                effect.Parameters.MaxSat = treeSettings?.SpecialGroupMaximumSaturationValue ?? 1;
+
+                effect.Parameters.MinHue = treeSettings?.SpecialGroupMinimalHueValue ?? 0;
+                effect.Parameters.MaxHue = treeSettings?.SpecialGroupMaximumHueValue ?? 1;
+
+                effect.Apply();
+
+                sb.Draw(drawData);
+            }
+        }
+        sb.End();
     }
 }
 
